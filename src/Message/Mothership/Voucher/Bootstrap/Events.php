fb1545d942d05d89a498d1e9b919ed5cb02e9e4c
Joe Holdcroft <joe@message.co.uk>
2013-08-21 17:09:04 +0100

    Adding menu item for vouchers

--- src/Message/Mothership/Voucher/Bootstrap/Events.php
+++ src/Message/Mothership/Voucher/Bootstrap/Events.php
@@ -4,6 +4,8 @@ namespace Message\Mothership\Voucher\Bootstrap;
 
 use Message\Mothership\Voucher;
 
+use Message\Mothership\ControlPanel\Event\Event;
+
 use Message\Cog\Bootstrap\EventsInterface;
 
 class Events implements EventsInterface
@@ -11,5 +13,11 @@ class Events implements EventsInterface
 	public function registerEvents($dispatcher)
 	{
 		$dispatcher->addSubscriber(new Voucher\EventListener);
+
+		$dispatcher->addListener(Event::BUILD_MAIN_MENU, function($event) {
+			$event->addItem('ms.cp.voucher.index', 'Vouchers', array(
+				'ms.cp.voucher'
+			));
+		});
 	}
 }
