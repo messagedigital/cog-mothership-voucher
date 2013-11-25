dc57b53faaf8cb2c0c36904381dab996c667c72d
Laurence Roberts <lsjroberts@gmail.com>
2013-11-25 16:19:02 +0000

    Updated routes to use fully qualified namespace

--- src/Message/Mothership/Voucher/Bootstrap/Routes.php
+++ src/Message/Mothership/Voucher/Bootstrap/Routes.php
@@ -8,21 +8,21 @@ class Routes implements RoutesInterface
 {
 	public function registerRoutes($router)
 	{
-		$router['ms.voucher']->add('ms.voucher.process', '/voucher/add', '::Controller:AddVoucher#voucherProcess');
+		$router['ms.voucher']->add('ms.voucher.process', '/voucher/add', 'Message:Mothership:Voucher::Controller:AddVoucher#voucherProcess');
 
 		$router['ms.cp.voucher']->setParent('ms.cp')->setPrefix('/voucher');
 
-		$router['ms.cp.voucher']->add('ms.cp.voucher.index', '', '::Controller:ControlPanel#index');
-		$router['ms.cp.voucher']->add('ms.cp.voucher.create.action', '/create', '::Controller:ControlPanel#createAction')
+		$router['ms.cp.voucher']->add('ms.cp.voucher.index', '', 'Message:Mothership:Voucher::Controller:ControlPanel#index');
+		$router['ms.cp.voucher']->add('ms.cp.voucher.create.action', '/create', 'Message:Mothership:Voucher::Controller:ControlPanel#createAction')
 			->setMethod('POST');
-		$router['ms.cp.voucher']->add('ms.cp.voucher.create', '/create', '::Controller:ControlPanel#create');
-		$router['ms.cp.voucher']->add('ms.cp.voucher.search', '/search', '::Controller:ControlPanel#search')
+		$router['ms.cp.voucher']->add('ms.cp.voucher.create', '/create', 'Message:Mothership:Voucher::Controller:ControlPanel#create');
+		$router['ms.cp.voucher']->add('ms.cp.voucher.search', '/search', 'Message:Mothership:Voucher::Controller:ControlPanel#search')
 			->setMethod('POST');
 
-		$router['ms.cp.voucher']->add('ms.cp.voucher.invalidate', '/{id}/invalidate', '::Controller:ControlPanel#invalidate')
+		$router['ms.cp.voucher']->add('ms.cp.voucher.invalidate', '/{id}/invalidate', 'Message:Mothership:Voucher::Controller:ControlPanel#invalidate')
 			->setRequirement('id', '[A-Z0-9]+')
 			->setMethod('DELETE');
 
-		$router['ms.cp.voucher']->add('ms.cp.voucher.view', '/{id}', '::Controller:ControlPanel#view');
+		$router['ms.cp.voucher']->add('ms.cp.voucher.view', '/{id}', 'Message:Mothership:Voucher::Controller:ControlPanel#view');
 	}
 }
