f619d408abb1c4813fb5b2efe3802c96695ba422
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-11-25 12:25:39 +0000

    docblock exception classes

--- src/Exception/EVoucherSendException.php
+++ src/Exception/EVoucherSendException.php
@@ -2,6 +2,15 @@
 
 namespace Message\Mothership\Voucher\Exception;
 
+/**
+ * Class EVoucherSendException
+ * @package Message\Mothership\Voucher\Exception
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * Exception to throw when an e-voucher cannot be distributed. Implements Cog's TranslationExceptionInterface
+ * so takes a second parameter of a translation string, and a third parameter of translation params.
+ */
 class EVoucherSendException extends VoucherDisplayException
 {
 
--- src/Exception/VoucherDisplayException.php
+++ src/Exception/VoucherDisplayException.php
@@ -4,6 +4,14 @@ namespace Message\Mothership\Voucher\Exception;
 
 use Message\Cog\Exception\TranslationLogicException;
 
+/**
+ * Class VoucherDisplayException
+ * @package Message\Mothership\Voucher\Exception
+ *
+ * @author  Thomas Marchant <thomas@mothership.ec>
+ *
+ * General exception for displaying voucher-related errors as flash messages to users
+ */
 class VoucherDisplayException extends TranslationLogicException
 {
 
